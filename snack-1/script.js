a336f09be9e2a76c7e470f1ca59b6c157886fb41
BkStyle89 <kajzer@hotmail.it>
2026-02-20 15:23:54 +0100

    snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,15 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+let nomi =[]
+names.forEach (name => {
+    console.log(name);
+    nomi.push(name)
+    
+});
+console.log(nomi.join(" "));
+
+
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
