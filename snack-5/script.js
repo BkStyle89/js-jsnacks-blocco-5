c194fb28ae73951e7f3bf181490d74d2491345cd
BkStyle89 <kajzer@hotmail.it>
2026-02-20 15:54:19 +0100

    snack 5 Filter

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,15 +2,26 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 //ciclo For
 
-let evenNumbers=[]
+/* let evenNumbers=[] */
 
-for(let i = 0; i< nums.length;i++){
+/* for(let i = 0; i< nums.length;i++){
     const num = nums[i]
     console.log(num);
     if(num %2==0){
         evenNumbers.push(num)
     }
 }
+console.log(evenNumbers);
+ */
+
+//Filter
+
+const evenNumbers = nums.filter(num =>{
+    if(num %2===0){
+        return true
+    }
+    return false
+})
 console.log(evenNumbers);
 
 
