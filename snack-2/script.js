85bf47e9540d0faa787e0b356a39b2e4ddf0d279
BkStyle89 <kajzer@hotmail.it>
2026-02-20 15:30:13 +0100

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,5 +4,14 @@ const people = [
   { name: 'Marco', age: 67 }
 ];
 
+let names = []
+for(let i=0; i< people.length; i++){
+  const person = people[i]
+  console.log(person.name);
+  names.push(person.name)
+  
+}
+console.log(names.join(" "));
+
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
