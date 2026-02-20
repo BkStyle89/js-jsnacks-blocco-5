eea7b2455937b217aa616a6a2aa07b591056a0da
BkStyle89 <kajzer@hotmail.it>
2026-02-20 16:29:13 +0100

    snack 8 ciclo for

--- snack-8/script.js
+++ snack-8/script.js
@@ -19,5 +19,18 @@ const students = [
   },
 ];
 
+
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+//ciclo For
+
+const classeStudente=[]
+for(let i=0;i< students.length;i++){
+  const student=students[i]
+  console.log(student);
+  if(student.id==1){
+    classeStudente.push(student.class)
+  }
+}
+console.log(classeStudente);
