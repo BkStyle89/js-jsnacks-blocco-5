5c118cad4936b23a13e60c9ed187a9147d20af7e
BkStyle89 <kajzer@hotmail.it>
2026-02-20 15:38:39 +0100

    snack 3 CicloFor

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,15 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+//for
+const oddNumbers =[]
+for(let i=0; i<numbers.length;i++){
+    const number=numbers[i]
+    if(number %2!==0){
+        oddNumbers.push(number)
+    }
+}
+console.log(oddNumbers);
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
