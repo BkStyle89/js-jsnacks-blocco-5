6882d4966a5f7828a6b2c435a786e3b161c76893
BkStyle89 <kajzer@hotmail.it>
2026-02-20 15:52:05 +0100

    snack 5 ciclo for

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,19 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+//ciclo For
+
+let evenNumbers=[]
+
+for(let i = 0; i< nums.length;i++){
+    const num = nums[i]
+    console.log(num);
+    if(num %2==0){
+        evenNumbers.push(num)
+    }
+}
+console.log(evenNumbers);
+
+
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
