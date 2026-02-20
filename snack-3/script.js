6508b3c888691e1c40dd8d14db824a1f7383472e
BkStyle89 <kajzer@hotmail.it>
2026-02-20 15:40:36 +0100

    snack 3 Filter

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,14 +1,25 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 //for
-const oddNumbers =[]
+/* const oddNumbers =[]
 for(let i=0; i<numbers.length;i++){
     const number=numbers[i]
     if(number %2!==0){
         oddNumbers.push(number)
     }
 }
+console.log(oddNumbers); */
+
+//filter
+
+const oddNumbers = numbers.filter(number=>{
+    if(number %2!==0){
+        return true
+    }
+    return false
+})
 console.log(oddNumbers);
+
 
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
